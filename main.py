d9630990914f0c449773ebe18e1d0e7cc780ac3e
Gero Zayas <gerozayas@gmail.com>
2024-10-30 09:36:52 +0100

    Add avg price by neighborhood

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+rent_df = pd.read_csv("./assets/Barcelona_rent_price.csv")
+
+columns = rent_df.columns
+print(columns)
+
+avg_price_by_nbhood= rent_df[["District", "Neighbourhood", "Price"]].groupby("Neighbourhood")
+
+print(rent_df[["District", "Neighbourhood", "Price"]])
+
+print("-------------------")
+
+print(avg_price_by_nbhood)
+ 
